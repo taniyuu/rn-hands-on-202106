d642e030318fb3b5eb0dca959438299930b9618e
taniyuu <cmt.0801nonki@gmail.com>
2021-06-06 16:31:14 +0900

    chore

--- src/navigations/AuthStack.tsx
+++ src/navigations/AuthStack.tsx
@@ -7,7 +7,7 @@ import { SignUpScreen } from "../screens/SignUpScreen";
 const Stack = createStackNavigator();
 
 export const AuthStack = () => {
-  return <Stack.Navigator >
+  return <Stack.Navigator initialRouteName="Login" headerMode="none">
       <Stack.Screen name="Login" component={LoginScreen}/>
       <Stack.Screen name="SignUp" component={SignUpScreen}/>
   </Stack.Navigator>;
