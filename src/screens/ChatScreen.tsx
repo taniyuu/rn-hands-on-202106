c8b41e9d79238a764b6129db8f1c0b5d63a0b0eb
taniyuu <cmt.0801nonki@gmail.com>
2021-06-06 15:34:54 +0900

    refactor

---
+++ src/screens/ChatScreen.tsx
@@ -0,0 +1,58 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, View } from 'react-native';
+import { GiftedChat } from "react-native-gifted-chat";
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+});
+
+export const ChatScreen=()=> {
+  // 表示するチャットメッセージの配列
+  const [messages, setMessages] = React.useState([]);
+
+  React.useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
+  const onSend = React.useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <GiftedChat
+        messages={messages}
+        placeholder="メッセージを入力してください..."
+        onSend={onSend}
+        user={{
+          _id: 1,
+          name: "Me",
+          avatar: "https://placeimg.com/140/140/any",
+        }}
+      />
+      {Platform.OS === "android" && <KeyboardAvoidingView behavior="padding" />}
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
